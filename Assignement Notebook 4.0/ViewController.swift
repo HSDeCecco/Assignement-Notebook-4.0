255f244cd553518a400c79bcd40ba8dbb3ef0d89
HSDeCecco <beeHSD2720@gmail.com>
2017-12-04 10:37:23 -0500

    Initial Commit

---
+++ Assignement Notebook 4.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignement Notebook 4.0
+//
+//  Created by  on 12/4/17.
+//  Copyright © 2017 MadManApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
